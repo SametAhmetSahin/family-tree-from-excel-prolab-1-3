9137745e6452f1fb3572f2e81b4b883dde7de0e2
Samet Ahmet Şahin <samet.sahin@bgasecurity.com>
2022-12-18 14:27:29 +0300

    Added Roboto font, added recursive node adding to Node scene, drawing trees are kind of broken but kind of works

--- godot-gui/Node.gd
+++ godot-gui/Node.gd
@@ -2,27 +2,44 @@ extends Control
 
 
 #[id, name, surname, birthdate, spouse, motherName, fatherName, bloodType, profession, maritalStatus, maidenName, gender]
-
+var person
 var data = {"id":"id", "name":"name", "surname":"surname", "birthdate":"birthdate", "spouse":"spouse", "motherName":"motherName", "fatherName":"fatherName", "bloodType":"bloodType", "profession":"profession", "maritalStatus":"maritalStatus", "maidenName":"maidenName", "gender":"gender"}
 
-func set_data(newdata):
-	
-	data = newdata
-	
+
+func set_person(newperson):
+	person = newperson
 	update_labels()
 
+func add_node(person: Dictionary, position: Vector2):
+	var node_scene = load("res://Node.tscn")
+	var newnode = node_scene.instance()
+	newnode.set_person(person)
+	newnode.rect_position = position
+	add_child(newnode)
+	return newnode
+	#call_deferred("add_child", newnode)
+
+func generate_subnodes(startpos):
+		
+	var totalwidth = person["children"].size() * 240 + (person["children"].size()-1) * 80
+	var offset = Vector2(totalwidth/2, 150)
+	for child in person["children"]:
+		var childnode = add_node(child, startpos + offset)
+		offset += childnode.generate_subnodes(startpos + offset)
+	return Vector2(totalwidth/2, 150)
+	pass
+
 func update_labels():
-	$Panel/id.text = str(data["id"])
-	$Panel/fullname.text = data["name"] + " " + data["surname"]
-	$Panel/birthdate.text = data["birthdate"]
-	$Panel/bloodtype.text = data["bloodType"]
-	$Panel/profession.text = data["profession"]
-	$Panel/maidenname.text = data["maidenName"]
+	$Panel/id.text = str(person["data"]["id"])
+	$Panel/fullname.text = person["data"]["name"] + " " + person["data"]["surname"]
+	$Panel/birthdate.text = person["data"]["birthdate"]
+	$Panel/bloodtype.text = person["data"]["bloodType"]
+	$Panel/profession.text = person["data"]["profession"]
+	$Panel/maidenname.text = person["data"]["maidenName"]
 	
-	print(data["gender"])
-	# mavi 00b1ff
-	#if data["gender"] == 0
-	
-	#$Panel.self_modulate = Color("00b1ff")
-
-
+	#print(person["data"]["gender"])
+	#mavi 00b1ff
+	if person["data"]["gender"]:
+		$Panel.self_modulate = Color("00b1ff")
+	else:
+		$Panel.self_modulate = Color("f700ff")
--- godot-gui/TreeView.gd
+++ godot-gui/TreeView.gd
@@ -4,14 +4,29 @@ onready var requests = $Requests
 
 onready var node_scene = preload("res://Node.tscn")
 
-func add_node(data: Dictionary, position: Vector2):
+func generate_tree(person: Dictionary, startpos: Vector2):
+	#startpos = startpos
+	var totalwidth = person["children"].size() * 120 + (person["children"].size()-1) * 80
+	var offset = Vector2(totalwidth, 100)
+	#print(person["children"])
+	for child in person["children"]:
+		print(child.keys())
+		var childnode = add_node(child, startpos + offset)
+		offset += childnode.generate_subnodes(startpos + offset)
+		offset += Vector2(200, 0)
+
+
+func add_node(person: Dictionary, position: Vector2):
 	var newnode = node_scene.instance()
-	newnode.set_data(data)
+	newnode.set_person(person)
 	newnode.rect_position = position
-	add_child(newnode)
+	$Tree.call_deferred("add_child", newnode)
+	return newnode
+	#add_child(newnode)
 
 func wait_for_request(req):
 	yield(req, "request_completed")
+
 
 func _ready():
 	var get_req = requests.get_request("http://localhost:8080/gettree")
@@ -20,19 +35,17 @@ func _ready():
 	#print("response: " + str(response_decoded) + " endresponse")
 	var parse_result: JSONParseResult = JSON.parse(response_decoded)
 	var result = parse_result.result
-	var x = 0
-	var y = 0
+	var pos = Vector2(0, 0)
 	for root in result:
 		#print(root["rootNode"]["data"].keys())
 		#print(str(root["rootNode"]["data"]["id"]) + " " + root["rootNode"]["data"]["name"] + " " + root["rootNode"]["data"]["surname"])
-		var pos = Vector2(x, y)
-		x += 100
-		y += 200
-		var person = root["rootNode"]["data"]
-		var data = {"id": person["id"], "name": person["name"], "surname": person["surname"], "birthdate": person["birthdate"], "spouse": person["spouse"], "motherName": person["motherName"], "fatherName": person["fatherName"], "bloodType": person["bloodType"], "profession": person["profession"], "maritalStatus": person["maritalStatus"], "maidenName": person["maidenName"], "gender": person["gender"]}
-
-		add_node(data, pos)
-	
+		var person = root["rootNode"]
+		#var data = {"id": person["data"]["id"], "name": person["data"]["name"], "surname": person["data"]["surname"], "birthdate": person["data"]["birthdate"], "spouse": person["data"]["spouse"], "motherName": person["data"]["motherName"], "fatherName": person["data"]["fatherName"], "bloodType": person["data"]["bloodType"], "profession": person["data"]["profession"], "maritalStatus": person["data"]["maritalStatus"], "maidenName": person["data"]["maidenName"], "gender": person["data"]["gender"]}
+		pos += Vector2(300, 0)
+		#add_node(data, pos)
+		
+		generate_tree(person, Vector2(0, 0))
+		break
 	
 
 
